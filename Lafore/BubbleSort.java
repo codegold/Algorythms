dcc2f2bba664d9bbdfcabde5f3a79172726cabca
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-04-03 15:45:06 +0300

    Add files via upload

--- Lafore/BubbleSort.java
+++ Lafore/BubbleSort.java
@@ -1,17 +1,23 @@
 package Lafore;
 
+import MyTests.MyBubbleSort;
+
+/**
+ Bubble sort realizing.
+*/
+
 
 class ArrayBub {
     private int[] a;                                         // Link to array
     private int nElems;                                //Quantity of elements
 
-//-------------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 
     public ArrayBub(int max) {                                  //Constructor
         a = new int[max];
         nElems = 0;                                           //Now no arrays
     }
-//-------------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 
     public void insert(int value) {
         a[nElems] = value;                                       // Inserting
@@ -25,16 +31,15 @@ class ArrayBub {
         System.out.println();
     }
 
-//-------------------------------------------------------------------------------
-
+//---------------------------------------------------------------------------
     public void bubbleSort() {
         int in, out;
-        for (out = nElems - 1; out > 1; out--)             //Outer cycle back
+        for (out = nElems - 1; out > 0; out--)             //Outer cycle back
             for (in = 0; in < out; in++)               //Inner cycle straight
-                if (a[in] > a[in + 1])
-                    swap(in, in + 1);
+                if (a[in] > a[in + 1])                        //Order broken?
+                    swap(in, in + 1);                    //Change places
     }
-//-------------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 
     private void swap(int one, int two) {
         int temp = a[one];
@@ -43,11 +48,11 @@ class ArrayBub {
     }
 }
 
-/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 class BubbleSortApp {
     public static void main(String[] args) {
-        int maxSize = 100;
-        ArrayBub arr = new ArrayBub(maxSize);
+
+        ArrayBub arr = new ArrayBub(100);       //Create array with size
 
         arr.insert(77);
         arr.insert(99);
@@ -64,8 +69,25 @@ class BubbleSortApp {
 
         arr.bubbleSort();
         arr.display();
+
+        ArrayBub arr2 = new ArrayBub(5);
+        arr2.insert(77);
+        arr2.insert(90);
+        arr2.insert(33);
+        arr2.insert(44);
+        arr2.insert(10);
+        //arr2.insert(10);
+        System.out.println();
+        arr2.display();
+        arr2.bubbleSort();
+        System.out.println();
+        arr2.display();
     }
+
+
+
+
 }
-/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 
