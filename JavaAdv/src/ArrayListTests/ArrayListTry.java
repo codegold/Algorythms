df934991bb58970433e7a8ed800c216587bcebb5
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-05-21 10:18:55 +0300

    Add files via upload

---
+++ JavaAdv/src/ArrayListTests/ArrayListTry.java
@@ -0,0 +1,14 @@
+package ArrayListTests;
+
+import java.util.ArrayList;
+
+public class ArrayListTry {
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> myArrList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            myArrList.add(i);
+        }
+    }
+}
