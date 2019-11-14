5bf9902e7e351b9e4f45dd368b0349d2296f3fdd
«MaxRubshik» <rubshikmax@gmail.com>
2019-11-14 15:31:17 +0300

    selectSort.java

---
+++ java/src/SelectSort/ArraySel.java
@@ -0,0 +1,39 @@
+package src.SelectSort;
+
+public class ArraySel {
+    private long[] a; // Ссылка на массив a
+    private int nElems; // Количество элементов данных
+
+    public ArraySel(int max) {// Конструктор
+        a = new long[max];// Создание массива
+        nElems = 0; // Пока нет ни одного элемента
+    }
+
+    public void insert(long value) {
+        a[nElems] = value;
+        nElems++;
+    }
+
+    public void display() {
+        for (int i = 0; i < nElems; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void selectionSort() {
+        int in, out, min;
+        for (out = 0; out < nElems - 1; out++) {
+            min = (int) a[0];
+            for (in = 1; in < nElems; in++) {
+                if (a[in] < min)
+                    a[in] = min;
+                int temp = (int) a[in];
+                a[in] = min;
+                min = temp;
+
+            }
+        }
+    }
+
+}
