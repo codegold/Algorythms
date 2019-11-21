04391446d4abb390df39f7c0cb9056e4a4328fad
«MaxRubshik» <rubshikmax@gmail.com>
2019-11-21 18:54:44 +0300

    Stack package. Methods peek() isEmpty() isFull() added

--- java/src/Stack/StackX.java
+++ java/src/Stack/StackX.java
@@ -5,18 +5,31 @@ public class StackX {
     private long[] stackArray;
     private int top;
 
-    public StackX(int s){
+    public StackX(int s) {
         maxSize = s; //define size
         stackArray = new long[maxSize]; //init array
         top = -1;
     }
 
-    public void push(long j){ // put elem on the top
+    public void push(long j) { // put elem on the top
         stackArray[++top] = j;
 
     }
 
-    public long pop(){
+    public long pop() {
         return stackArray[top--]; //extraction elem & reduce top
     }
+
+    public long peek() {
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+
 }
