b59c4fbea0f043c3b9a2907009aab3cfb8b5ab13
«MaxRubshik» <rubshikmax@gmail.com>
2019-11-22 11:12:06 +0300

    Reverse app. Added StackX class

--- java/src/Stack/StackApp.java
+++ java/src/Stack/StackApp.java
@@ -9,6 +9,7 @@ public class StackApp {
         stackX.push(60);
         stackX.push(80);
         stackX.push(100);
+        System.out.println(stackX.peek());
 
         while (!stackX.isEmpty()) {
             long value = stackX.pop();
@@ -17,13 +18,16 @@ public class StackApp {
         }
         System.out.println();
 
-        while (!stackX.isFull()) {
-            int newValue = (int) (Math.random()*100);
-            stackX.push(newValue);
-            System.out.print(newValue);
-            System.out.print(" ");
-        }
-        System.out.println();
+//        while (!stackX.isFull()) {
+//            int newValue = (int) (Math.random()*100);
+//            stackX.push(newValue);
+//            System.out.print(newValue);
+//            System.out.print(" ");
+//        }
+//        System.out.println();
+        stackX.push(33);
+        stackX.push(34);
+        System.out.println(stackX.peek());
 
 
     }
