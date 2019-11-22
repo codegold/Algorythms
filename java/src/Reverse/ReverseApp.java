d2dca41bed1aa883468469af3e750f3809dad730
«MaxRubshik» <rubshikmax@gmail.com>
2019-11-22 12:01:51 +0300

    ReverseApp class added

---
+++ java/src/Reverse/ReverseApp.java
@@ -0,0 +1,29 @@
+package src.Reverse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverseApp {
+    public static void main(String[] args) throws IOException {
+        String input, output;
+        while (true){
+            System.out.print("Enter a string: ");
+            System.out.flush();
+            input = getString();
+            if(input.equals(""))
+                break;
+
+            Reverser reverser = new Reverser(input);
+            output = reverser.doReverse();
+            System.out.println("Reversed: "+ output);
+        }
+    }
+
+    private static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
+    }
+}
