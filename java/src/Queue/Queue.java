9a446c00a5db29f60a8a2e8a5108962bcb641385
«MaxRubshik» <rubshikmax@gmail.com>
2019-11-23 15:08:51 +0300

    Methods peekFront() isEmpty() isFull() size() added

--- java/src/Queue/Queue.java
+++ java/src/Queue/Queue.java
@@ -22,13 +22,31 @@ public class Queue {
         nItems++;
     }
 
-    public long remove(){
+    public long remove() {
         long temp = queArray[front++];
-        if(front == maxSize)
+        if (front == maxSize)
             front = 0;
         nItems--;
         return temp;
     }
+
+    public long peekFront() {
+        return queArray[front];
+    }
+
+    public boolean isEmpty() {
+        return nItems == 0;
+    }
+
+    public boolean isFull() {
+        return nItems == maxSize;
+    }
+
+    public int size() {
+        return nItems;
+    }
+
+
 
 }
 
