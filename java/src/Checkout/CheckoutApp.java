86824dfc9e2a0f70021214c2c586678061674ac0
«MaxRubshik» <rubshikmax@gmail.com>
2019-12-04 17:24:10 +0300

    CheckOutApp added

---
+++ java/src/Checkout/CheckoutApp.java
@@ -0,0 +1,33 @@
+package src.Checkout;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CheckoutApp {
+    public static void main(String[] args) throws IOException {
+        String input;
+        Checkout grocery = new Checkout(3, 20, true);
+        grocery.initialize();
+        grocery.display();
+
+        while (true) {
+            System.out.print("Time to Pass: ");
+            System.out.flush();
+            input = getString();
+
+            if (input.equals("")) break;
+            grocery.process(Integer.parseInt(input));
+
+            grocery.display();
+        }
+    }
+
+    public static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
+    }
+}
