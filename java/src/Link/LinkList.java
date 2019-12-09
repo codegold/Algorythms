64b4a4888f7c4caa31bf3f296b4c02326278647b
«MaxRubshik» <rubshikmax@gmail.com>
2019-12-09 15:41:02 +0300

    LinkList class added (realizing linked list)

---
+++ java/src/Link/LinkList.java
@@ -0,0 +1,28 @@
+package src.Link;
+
+public class LinkList {
+    private Link first; //Link to first elem
+
+    public void LinkList() { //List still has no elems
+        first = null;
+    }
+
+    public boolean isEmpty() {
+        return (first == null);
+    }
+
+    public void insertFirst(int id, double dd) {
+        Link newLink = new Link(id, dd);
+        newLink.next = first;
+        first = newLink;
+    }
+
+    public Link deleteFirst() {
+        Link temp = first;
+        first = first.next;
+        return temp;
+    }
+
+
+
+}
