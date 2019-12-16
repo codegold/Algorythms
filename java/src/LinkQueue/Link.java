71ffb4e8e78009ee336fccc1f2d8651ac362a742
«MaxRubshik» <rubshikmax@gmail.com>
2019-12-16 12:16:52 +0300

    LinkQueue app. class Link added(realization via linked list)

---
+++ java/src/LinkQueue/Link.java
@@ -0,0 +1,15 @@
+package src.LinkQueue;
+
+public class Link {
+    public long dData;
+    public Link next;
+
+    public Link(long d){
+        dData = d;
+    }
+
+    public void displayLink() {
+        System.out.print(dData + " ");
+    }
+
+}
