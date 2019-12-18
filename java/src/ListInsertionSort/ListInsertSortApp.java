721f909972b146d6881d0887e27f5529e951e62d
«MaxRubshik» <rubshikmax@gmail.com>
2019-12-18 16:42:20 +0300

    ListInsertionSortApp class (in process)

--- java/src/ListInsertionSort/ListInsertSortApp.java
+++ java/src/ListInsertionSort/ListInsertSortApp.java
@@ -3,18 +3,22 @@ package src.ListInsertionSort;
 public class ListInsertSortApp {
     public static void main(String[] args) {
         int size = 10;
+
         Link[] linkArray = new Link[size];
 
         for (int i = 0; i < size; i++) {
             int n = (int) (java.lang.Math.random() * 99);
+
             Link newLink = new Link(n);
             linkArray[i] = newLink;
         }
         System.out.print("Unsorted array: ");
+
         for (int i = 0; i < size; i++)
             System.out.print(linkArray[i].dData + " ");
 
         System.out.println("");
+
         // Создание нового списка
         // инициализированного содержимым массива
         SortedList theSortedList = new SortedList(linkArray);
@@ -23,8 +27,10 @@ public class ListInsertSortApp {
             linkArray[j] = theSortedList.remove();
 
         System.out.print("Sorted Array: ");
+
         for (int k = 0; k < size; k++)
             System.out.print(linkArray[k].dData + " ");
+
         System.out.println();
 
 
