82f23ec48572aad5a05034c2237d255a4b368095
«MaxRubshik» <rubshikmax@gmail.com>
2019-12-23 23:26:44 +0300

    ChapterFive (app is here) class added.

---
+++ java/src/PriorQueue/ChapterFive.java
@@ -0,0 +1,45 @@
+package src.PriorQueue;
+
+public class ChapterFive {
+    public static void main(String[] args) {
+        LaforePriorityQueue myQueue = new LaforePriorityQueue();
+        myQueue.insert(55);
+        myQueue.insert(77);
+        myQueue.insert(11);
+        myQueue.insert(99);
+
+        myQueue.display();
+
+        long temp = myQueue.peekMin();
+        System.out.println("min is: " + temp);
+
+        myQueue.remove();
+        myQueue.remove();
+
+        myQueue.display();
+
+        myQueue.insert(444);
+        myQueue.insert(888);
+        myQueue.insert(555);
+
+        myQueue.display();
+
+        Link answer = myQueue.remove();
+        System.out.println(answer.dData);
+
+        answer = myQueue.remove();
+        System.out.println(answer.dData);
+
+        answer = myQueue.remove();
+        System.out.println(answer.dData);
+
+        answer = myQueue.remove();
+        System.out.println(answer.dData);
+
+//        answer = myQueue.remove();
+//        System.out.println(answer.dData);
+
+        System.out.println("Finally: ");
+        myQueue.display();
+    }
+}
