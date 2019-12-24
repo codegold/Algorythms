eaa8ff22e52afd0408f1870046d68fbd0dab7dda
«MaxRubshik» <rubshikmax@gmail.com>
2019-12-24 17:38:13 +0300

    CStack class added. Task to realize Stack on the base of CycleLinkedList.

---
+++ java/src/CStack/CStack.java
@@ -0,0 +1,33 @@
+package src.CStack;
+
+import src.CycleLinkedList.CycleLinkedList;
+
+import java.util.EmptyStackException;
+
+public class CStack {
+    private CycleLinkedList theList;
+
+    public CStack() {
+
+        theList = new CycleLinkedList();
+        theList.insert(-1);//the first element is an anchor
+        //not used in stack operations
+    }
+
+    public void push(long value) {
+        theList.insert(value);
+    }
+
+    public long pop() {
+        if (theList.getItems() == 1) {
+            System.out.println("Stack is empty");
+            throw new EmptyStackException();
+        } else {
+            return theList.delete().dData;
+        }
+    }
+
+    public long peek() {
+        return theList.peek().dData;
+    }
+}
