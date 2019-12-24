71391156272d736bfc3b9a6954256d06d9310587
«MaxRubshik» <rubshikmax@gmail.com>
2019-12-24 17:43:39 +0300

    CStackApp class added.

---
+++ java/src/CStack/CStackApp.java
@@ -0,0 +1,16 @@
+package src.CStack;
+
+public class CStackApp {
+    public static void main(String[] args) {
+        CStack theStack = new CStack();
+        theStack.push(1);
+        theStack.push(5);
+        theStack.push(8);
+        theStack.push(2);
+
+        System.out.println(theStack.pop());
+        System.out.println(theStack.pop());
+        System.out.println(theStack.pop());
+        System.out.println(theStack.pop());
+    }
+}
