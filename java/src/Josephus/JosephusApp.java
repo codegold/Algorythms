1c342bf9ca9e129063773030649674215a6c7b28
«MaxRubshik» <rubshikmax@gmail.com>
2019-12-25 13:25:04 +0300

    JosephusApp class added.

---
+++ java/src/Josephus/JosephusApp.java
@@ -0,0 +1,9 @@
+package src.Josephus;
+
+public class JosephusApp {
+    public static void main(String[] args) {
+
+        Josephus trial = new Josephus(10,2,1);
+        trial.execute();
+    }
+}
