17f6f231000b4c7f3a77d67e6d7a55a076b7c47a
«MaxRubshik» <rubshikmax@gmail.com>
2019-12-26 13:52:22 +0300

    Recursion. Triangle app added.

--- java/src/Matrix/MatrixApp.java
+++ java/src/Matrix/MatrixApp.java
@@ -3,7 +3,7 @@ package src.Matrix;
 public class MatrixApp {
     public static void main(String[] args) {
 
-        Matrix theMatrix = new Matrix(4,6);
+        Matrix theMatrix = new Matrix(4, 6);
         theMatrix.display();
         //this is just for test
 
