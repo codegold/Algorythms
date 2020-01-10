6741f48210da44038df8e7d6c39e9989dd030f70
«MaxRubshik» <rubshikmax@gmail.com>
2020-01-10 11:29:41 +0300

    displayWord() added

--- java/src/Anagram/AnagramApp.java
+++ java/src/Anagram/AnagramApp.java
@@ -32,6 +32,17 @@ public class AnagramApp {
 
 
     private static void displayWord() {
+        if (count < 99)
+            System.out.print(" ");
+        if (count < 9)
+            System.out.print(" ");
+        System.out.print(++count + " ");
+        for (int j = 0; j < size; j++)
+            System.out.print(arrChar[j]);
+        System.out.print(" ");
+        System.out.flush();
+        if (count % 6 == 0)
+            System.out.println(" ");
     }
 
     private static void rotate(int newSize) {
