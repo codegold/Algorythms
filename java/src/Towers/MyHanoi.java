8469b5452f9c196ca4112d9071e21b96be70ff7b
«MaxRubshik» <rubshikmax@gmail.com>
2020-01-30 10:30:05 +0300

    few edits

--- java/src/Towers/MyHanoi.java
+++ java/src/Towers/MyHanoi.java
@@ -4,18 +4,22 @@ public class MyHanoi {
     static int nDisks = 3;
 
     public static void main(String[] args) {
-        myHanoi(nDisks, 'A', 'B', 'C');
+        myHannoi(nDisks, 'A', 'B', 'C');
     }
 
-    public static void myHanoi(int nD, char from, char tmp, char to) {
-        if (nD == 1)
-            System.out.println("Disk 1 from: " + from + " to: " + to);
+    public static void myHanoi(int nDis, char a, char b, char c) {
+        myHannoi(nDisks, 'A', 'B', 'C');
+    }
 
-        else {
-            myHanoi(nD - 1, from, to, tmp);
-            System.out.println("disk " + nD + " from: " +from+ " to: " +to);
-
-            //myHanoi(nD-1, tmp, from, to);
+    public static void myHannoi(int nDis, char a, char b, char c) {
+        if (nDis == 1) {
+            System.out.println("Disk 1 moves from: " + a + " to: "
+                    + c);
+        } else {
+            myHannoi(nDis - 1, a, c, b);
+            System.out.println("Disk: " + nDis + " move from: " + a +
+                    " to: " + c);
+            myHannoi(nDis - 1, b, a, c);
         }
     }
 }
