98b2d11d82759ef117ca35c9cf045fe07ea1c016
«MaxRubshik» <rubshikmax@gmail.com>
2020-02-03 15:25:13 +0300

    class StackX finished

--- java/src/StackTriangle/StackX.java
+++ java/src/StackTriangle/StackX.java
@@ -3,9 +3,9 @@ package src.StackTriangle;
 public class StackX {
     private int maxSize;
     private Params[] stackArray;
-    private  int top;
+    private int top;
 
-    public StackX (int s) {
+    public StackX(int s) {
         maxSize = s;// Определение размера массива
         stackArray = new Params[maxSize];// Создание массива
         top = -1;// Массив пока не содержит элементов
@@ -14,4 +14,13 @@ public class StackX {
     public void push(Params p) { // Размещение элемента на вершине стека
         stackArray[++top] = p; // Увеличение top, вставка элемента
     }
+
+    public Params pop() { // Извлечение элемента с вершины стека
+        return stackArray[top--]; // Обращение к элементу, уменьшение top
+    }
+
+    public Params peek() {
+        return stackArray[top];  // Чтение элемента на вершине стека
+    }
+
 }
