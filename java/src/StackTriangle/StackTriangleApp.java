d080148f752dd7ee604d4f26837fb24a1343c1bc
«MaxRubshik» <rubshikmax@gmail.com>
2020-02-03 17:21:53 +0300

    step() added. Switches cases.

--- java/src/StackTriangle/StackTriangleApp.java
+++ java/src/StackTriangle/StackTriangleApp.java
@@ -1,4 +1,64 @@
 package src.StackTriangle;
 
 public class StackTriangleApp {
+    static int theNumber;
+    static int theAnswer;
+    static StackX theStack;
+    static int codePart;
+    static Params theseParams;
+
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        theNumber = getInt();
+        recTriangle();
+        System.out.println("Triangle = " + theAnswer);
+    }
+
+    private static void recTriangle() {
+        theStack = new StackX(10000);
+        codePart = 1;
+        while (step() == false) // Вызывать, пока step() не вернет true
+            ;// Пустое тело цикла
+    }
+
+    public static boolean step() {
+        switch (codePart) {
+            case 1:// Исходный вызов
+                theseParams = new Params(theNumber, 6);
+                theStack.push(theseParams);
+                codePart = 2;
+                break;
+
+            case 2:// Вход в метод
+                theseParams = theStack.peek();
+                if (theseParams.n == 1) // Проверка
+                {
+                    theAnswer = 1;
+                    codePart = 5;
+                } else
+                    codePart = 3; // Рекурсивный вызов
+                break;
+
+            case 3:
+                Params newParams = new Params(theseParams.n - 1, 4);
+                theStack.push(newParams);
+                codePart = 2;
+                break;
+
+            case 4:
+                theseParams = theStack.peek();
+                theAnswer = theAnswer + theseParams.n;
+                codePart = 5;
+                break;
+
+            case 5:
+                theseParams = theStack.peek();
+                codePart = theseParams.returnAdress;
+                theStack.pop();
+                break;
+            case 6:
+                return true;
+        }
+        return false;
+    }
 }
