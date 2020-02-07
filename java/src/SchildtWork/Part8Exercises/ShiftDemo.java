c36afaf223b0ce76201d7aafc6352559f3ac9418
«MaxRubshik» <rubshikmax@gmail.com>
2020-02-07 17:16:55 +0300

    Schildt. Shift (bite moves) Demo

---
+++ java/src/SchildtWork/Part8Exercises/ShiftDemo.java
@@ -0,0 +1,27 @@
+package src.SchildtWork.Part8Exercises;
+
+public class ShiftDemo {
+    public static void main(String[] args) {
+        int val = 1;
+
+        for (int i = 0; i < 8; i++) {
+            for (int t = 128; t > 0; t = t / 2) {
+                if ((val & t) != -0) System.out.print("1 ");
+                else System.out.print("0 ");
+            }
+            System.out.println();
+            val = val << 1; //move left
+        }
+        System.out.println();
+
+        val = 128;
+        for (int i = 0; i < 8; i++) {
+            for (int t = 128; t > 0; t = t / 2) {
+                if ((val & t) != 0) System.out.print("1 ");
+                else System.out.print("0 ");
+            }
+            System.out.println();
+            val = val >> 1; //move right
+        }
+    }
+}
