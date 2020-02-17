779554e4a6169dbe12601e5c118a6a83ff0826bb
«MaxRubshik» <rubshikmax@gmail.com>
2020-02-17 13:20:19 +0300

    Data base with file. Demo class added.

--- java/src/SchildtWork/Part10/Help.java
+++ java/src/SchildtWork/Part10/Help.java
@@ -3,6 +3,7 @@ package src.SchildtWork.Part10;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Help {
     String helpfile; //name of file
@@ -43,4 +44,34 @@ public class Help {
         }
         return false;
     }
+
+    String getSelection() {
+        String topic = "";
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Pick a topic: ");
+        try {
+            topic = br.readLine();
+        } catch (IOException e) {
+            System.out.println("Error while reading from console.");
+        }
+        return topic;
+    }
+
+    //DEMO
 }
+
+class FileHelp {
+    public static void main(String[] args) {
+            Help helpObj = new Help("helpfile.txt");
+            String topic;
+        System.out.println("Use system. /n " + "For exit press 'stop'.");
+        do {
+            topic = helpObj.getSelection();
+
+            if(!helpObj.helpon(topic))
+                System.out.println("Topic not found. \n");
+        }while (topic.compareTo("stop") != 0);
+    }
+}
+
