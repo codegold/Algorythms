9f429866c606ea6f0aaa7c09d616d755fe335f79
«MaxRubshik» <rubshikmax@gmail.com>
2020-02-18 16:59:22 +0300

    Three threads. Demo.

---
+++ java/src/SchildtWork/Part11Concurrency/ThreeThreads/MyThread.java
@@ -0,0 +1,45 @@
+package src.SchildtWork.Part11Concurrency.ThreeThreads;
+
+public class MyThread implements Runnable {
+    Thread thrd;
+
+    MyThread(String name) {
+        thrd = new Thread(this, name);
+        thrd.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println(thrd.getName() + " start");
+        try {
+            for (int count = 0; count < 10; count++) {
+                Thread.sleep(400);
+                System.out.println("B " + thrd.getName()
+                        + ", count " + count);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(thrd.getName() + " finishing.");
+    }
+}
+
+class MoreThreads {
+    public static void main(String[] args) {
+        System.out.println("Start of the main thread.");
+
+        MyThread mt1 = new MyThread("Child #1");
+        MyThread mt2 = new MyThread("Child #2");
+        MyThread mt3 = new MyThread("Child #3");
+
+        for (int i = 0; i < 60; i++) {
+            System.out.print(".");
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Finishing of the main.");
+    }
+}
