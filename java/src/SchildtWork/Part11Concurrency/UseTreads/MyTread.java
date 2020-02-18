c7844dc94c5cd7aa0bee0340bb9f2f15780bf4d0
«MaxRubshik» <rubshikmax@gmail.com>
2020-02-18 14:54:17 +0300

    Use treads demo. Improved.

--- java/src/SchildtWork/Part11Concurrency/UseTreads/MyTread.java
+++ java/src/SchildtWork/Part11Concurrency/UseTreads/MyTread.java
@@ -1,25 +1,45 @@
 package src.SchildtWork.Part11Concurrency.UseTreads;
 
 class MyTread implements Runnable {
-    String thrdName;
+    Thread thrd;
 
-    MyTread(String name){
-        thrdName = name;
+    MyTread(String name) {
+        thrd = new Thread(this, name);
+        thrd.start();
     }
 
-    //Point of entrance to thread
+    @Override
     public void run() {
-        System.out.println(thrdName + " - start");
+        System.out.println(thrd.getName() + " - start");
         try {
             for (int count = 0; count < 10; count++) {
-                Thread.sleep(400);
-                System.out.println("B "+ thrdName +
-                        " counter: " + count);
+                Thread.sleep(600);
+                System.out.println("B " + thrd.getName()
+                        + ", counter: " + count);
             }
         }
-        catch (InterruptedException ie){
-            System.out.println(thrdName + " - interrupted");
+        catch (InterruptedException ie) {
+            System.out.println(thrd.getName() + " canceled");
         }
-        System.out.println(thrdName + " finishing.");
+        System.out.println("Finishing of main thread.");
+    }
+}
+
+class UseThreadsImproved {
+    public static void main(String[] args) {
+        System.out.println("Start of the main thread.");
+
+        MyTread mt = new MyTread("Child #1");
+
+        for (int i = 0; i < 50; i++) {
+            System.out.print(".");
+            try{
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                //e.printStackTrace();
+                System.out.println("Interrupting main thread.");
+            }
+        }
+        System.out.println("Finishing of the main thread.");
     }
 }
