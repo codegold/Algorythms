c91e745500fb789aa579746a1cc41dfc36a4b389
«MaxRubshik» <rubshikmax@gmail.com>
2020-02-19 16:49:07 +0300

    Syncronized demo.

---
+++ java/src/SchildtWork/Part11Concurrency/SumArray/SumArray.java
@@ -0,0 +1,55 @@
+package src.SchildtWork.Part11Concurrency.SumArray;
+
+public class SumArray {
+    private int sum;
+
+    synchronized int sumArray(int nums[]) {
+        sum = 0; //null the sum
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            System.out.println("Current value of sum for "
+                    + Thread.currentThread().getName() +
+                    ": " + sum);
+
+            try {
+                Thread.sleep(10); //allow switch gear
+            } catch (InterruptedException e) {
+                System.out.println("Cancelling main thread.");
+            }
+        }
+        return sum;
+    }
+}
+
+class MyThread implements Runnable {
+    Thread thrd;
+    static SumArray sa = new SumArray();
+    int a[];
+    int answer;
+
+    //create new thread
+    MyThread(String name, int nums[]) {
+        thrd = new Thread(this, name);
+        a = nums;
+        thrd.start();
+    }
+
+    @Override
+    public void run() {
+        int sum;
+
+        System.out.println(thrd.getName() + " start");
+        answer = sa.sumArray(a);
+        System.out.println("SUM for " + thrd.getName() + ": " + answer);
+        System.out.println(thrd.getName() + " - finish");
+    }
+}
+
+class Sync {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5};
+        MyThread mt1 = new MyThread("Child #1", a);
+        MyThread mt2 = new MyThread("Child #2", a);
+    }
+}
