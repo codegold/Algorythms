adfb77b007ba00627289cecda0d7469e11b83b3c
«MaxRubshik» <rubshikmax@gmail.com>
2020-02-27 16:22:09 +0300

    Concurrency. Semaphore demo.

---
+++ java/src/SchildtWork/Part11Concurrency/Alishev/Syncronized/SingleTone/Test.java
@@ -0,0 +1,66 @@
+package src.SchildtWork.Part11Concurrency.Alishev.Syncronized.SingleTone;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Connection connection = Connection.getConnection();
+
+        for (int i = 0; i < 200; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        connection.work();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
+
+//Singleton
+class Connection {
+    private static Connection connection = new Connection();
+    private static int connectionsCount;
+    private Semaphore semaphore = new Semaphore(10);
+
+    private Connection() {
+
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public void work() throws InterruptedException {
+        semaphore.acquire();
+        try {
+            doWork();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private void doWork() throws InterruptedException {
+        synchronized (this) {
+            connectionsCount++;
+            System.out.println(connectionsCount);
+        }
+
+        Thread.sleep(3000);
+
+        synchronized (this) {
+            connectionsCount--;
+        }
+    }
+}
+
