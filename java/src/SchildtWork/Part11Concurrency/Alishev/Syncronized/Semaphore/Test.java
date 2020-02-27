5dd5b2129704f5b6d0dec73d78e7f6b55ca9b34f
«MaxRubshik» <rubshikmax@gmail.com>
2020-02-27 11:32:30 +0300

    Concurrency. Semaphore demo.

---
+++ java/src/SchildtWork/Part11Concurrency/Alishev/Syncronized/Semaphore/Test.java
@@ -0,0 +1,22 @@
+package src.SchildtWork.Part11Concurrency.Alishev.Syncronized.Semaphore;
+
+import java.util.concurrent.Semaphore;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore semaphore = new Semaphore(3);
+
+        semaphore.acquire();
+        semaphore.acquire();
+        semaphore.acquire();
+
+        System.out.println("All three acquired.");
+        semaphore.acquire();
+
+        System.out.println("No reach here");
+
+        semaphore.release();
+
+        System.out.println(semaphore.availablePermits());
+    }
+}
