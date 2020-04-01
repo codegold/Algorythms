2c4bdde0bcd33ebde86098b1face97101fde30a4
«MaxRubshik» <rubshikmax@gmail.com>
2020-04-01 23:15:50 +0300

    Reflection 2. Demo.

--- java/src/SchildtWork/JavaCollections/AnnotationThree/Person.java
+++ java/src/SchildtWork/JavaCollections/AnnotationThree/Person.java
@@ -34,4 +34,12 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- java/src/SchildtWork/JavaCollections/AnnotationThree/TestReflection.java
+++ java/src/SchildtWork/JavaCollections/AnnotationThree/TestReflection.java
@@ -1,13 +1,28 @@
 package JavaCollections.AnnotationThree;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Scanner;
 
 public class TestReflection {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Person person = new Person();
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        Scanner scanner = new Scanner(System.in);
+        Class classObject1 = Class.forName(scanner.next());
+        Class classObject2 = Class.forName(scanner.next());
+        String methodNam = scanner.next();
 
-        Class personClass = Person.class; //Класс персон - берем как объект класса класс и работаем как с обычным объектом
-        Class personClass2 = person.getClass();
+        Method m = classObject1.getMethod(methodNam, classObject2);
+        Object o1 = classObject1.newInstance();
+        Object o2 = classObject2.getConstructor(String.class).newInstance("String value");
+
+        m.invoke(o1, o2);
+
+        System.out.println(o1);
+//        Person person = new Person();
+//
+//        Class personClass = Person.class; //Класс персон - берем как объект класса класс и работаем как с обычным объектом
+//        Class personClass2 = person.getClass();
         //Class personClass3 = Class.forName("AnnotationThree.Person");
 
 //        Method[] methods = personClass.getMethods();
@@ -23,12 +38,12 @@ public class TestReflection {
 //                    + f.getType());
 //        }
 
-        Annotation[] annotations = personClass.getAnnotations();
-        for (Annotation a : annotations) {
-            if (a instanceof Author) {
-                System.out.println("Yes");
-            }
-
-        }
+//        Annotation[] annotations = personClass.getAnnotations();
+//        for (Annotation a : annotations) {
+//            if (a instanceof Author) {
+//                System.out.println("Yes");
+//            }
+//
+//        }
     }
 }
