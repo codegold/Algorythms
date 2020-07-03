7ab4b039632991a6e121d93edb88c7bdb2b200be
«MaxRubshik» <rubshikmax@gmail.com>
2020-07-03 12:17:43 +0300

    music player

---
+++ springapp1/src/main/java/ru/ali/springcourse/MusicPlayer.java
@@ -0,0 +1,14 @@
+package ru.ali.springcourse;
+
+public class MusicPlayer {
+    private Music music;
+
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
+
+}
--- springapp1/src/main/java/ru/ali/springcourse/TestSpring.java
+++ springapp1/src/main/java/ru/ali/springcourse/TestSpring.java
@@ -7,9 +7,10 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
+        Music music = context.getBean("musicBean", Music.class);
+
+        System.out.println(music.getSong());
         context.close();
     }
 }
-    
+
