11d1a578f4d0af653642f049c72838521732de7f
«MaxRubshik» <rubshikmax@gmail.com>
2020-07-06 16:36:29 +0300

    MusicPLayer update

--- springapp1/src/main/java/ru/ali/springcourse/MusicPlayer.java
+++ springapp1/src/main/java/ru/ali/springcourse/MusicPlayer.java
@@ -3,7 +3,34 @@ package ru.ali.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private String name;
+    private int volume;
+
+
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer() {
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
