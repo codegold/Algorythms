6a36cb5514ad54c37e28455348072a481d374e30
«MaxRubshik» <rubshikmax@gmail.com>
2020-07-08 22:36:07 +0300

    lesson 8 Bean lifecycle.

--- springapp1/src/main/java/ru/ali/springcourse/ClassicalMusic.java
+++ springapp1/src/main/java/ru/ali/springcourse/ClassicalMusic.java
@@ -1,6 +1,12 @@
 package ru.ali.springcourse;
 
 public class ClassicalMusic implements Music {
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody.";
