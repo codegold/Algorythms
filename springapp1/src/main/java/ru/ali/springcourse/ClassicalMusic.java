fe2aa19b26a6fb494c92f73ac2e2c299510828ea
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-07-10 00:06:50 +0300

    lesson 9 annotations

--- springapp1/src/main/java/ru/ali/springcourse/ClassicalMusic.java
+++ springapp1/src/main/java/ru/ali/springcourse/ClassicalMusic.java
@@ -1,5 +1,8 @@
 package ru.ali.springcourse;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
--- springapp1/src/main/java/ru/ali/springcourse/TestSpring.java
+++ springapp1/src/main/java/ru/ali/springcourse/TestSpring.java
@@ -7,11 +7,15 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("musicBean", Music.class);
+        Music music1 = context.getBean("rockMusic", Music.class);
+        Music music2 = context.getBean("classicalMusic", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
+        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
 
-        musicPlayer.playMusic();
+        musicPlayer1.playMusic();
+        musicPlayer2.playMusic();
+
 
         context.close();
     }
