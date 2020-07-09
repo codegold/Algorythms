ee624392f17fdc41bbcfada564504927bf78c45d
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-07-09 23:54:15 +0300

    lesson 9 prepare

--- springapp1/src/main/java/ru/ali/springcourse/ClassicalMusic.java
+++ springapp1/src/main/java/ru/ali/springcourse/ClassicalMusic.java
@@ -1,6 +1,11 @@
 package ru.ali.springcourse;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
     public void doMyInit(){
         System.out.println("Doing my initialization");
     }
